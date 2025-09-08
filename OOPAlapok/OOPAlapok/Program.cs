9b3b33ab84f67cc57dd80ea3857adae1fcbb38bf
Somogyik <Somogyik>
2025-09-08 12:15:20 +0200

    2 feladat done

--- OOPAlapok/OOPAlapok/Program.cs
+++ OOPAlapok/OOPAlapok/Program.cs
@@ -13,16 +13,26 @@ namespace OOPAlapok
         public string nev = "István";
         public int kor = 22;
 
-        
-    }
+        public Szemely(string Nev, int Kor) 
+        {
+            this.nev = Nev;
+            this.kor = Kor;
+        }
 
+
+        public string Kiir()
+        {
+            return $"A személy neve {nev} és életkor: {kor}";
+        }
+    }
+    
 
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely person = new Szemely();
+            Szemely person = new Szemely("Ibolya",25);
             Console.WriteLine($"A személy neve:  { person.nev} és életkora: {person.kor}");
         }
     }
