c47e931826c39f33f0c22d2d97138ac6389f9a8e
Somogyik <Somogyik>
2025-09-08 11:53:56 +0200

    1 feladat done

--- OOPAlapok/OOPAlapok/Program.cs
+++ OOPAlapok/OOPAlapok/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace OOPAlapok
 {
+    public class Szemely
+    {
+        
+        
+        public string nev = "István";
+        public int kor = 22;
+
+        
+    }
+
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely person = new Szemely();
+            Console.WriteLine($"A személy neve:  { person.nev} és életkora: {person.kor}");
         }
     }
 }
